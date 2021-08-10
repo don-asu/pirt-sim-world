430bebfd240192743b7a6bd29d66679509c41032
Don_ASU <dbalanza@asu.edu>
2021-08-10 14:44:07 -0700

    Added objects and script
    Added a gameobject for cube, plane, and a new script to handle speed.;

---
+++ PIRT_simulation_lab/Assets/Scripts/NewScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 6f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
